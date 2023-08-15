2c5b9f1bc805b01a580a8357823d4f26cd6f48d8
adnanbezerra <adnanbezerra@proton.me>
2023-08-15 19:50:25 -0300

    resolver primeira questão

---
+++ condicional/Questao1.java
@@ -0,0 +1,12 @@
+public class Questao1 {
+  public static void main(String[] args) {
+    int i = 0;
+    int j = 1;
+  
+    if (i > j) {
+      System.out.println("primeiro");
+    } else {
+      System.out.println("segundo");
+    }
+  }   
+}
