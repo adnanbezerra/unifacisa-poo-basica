8eab16f26281f04e4b230f5a7717c851ead7767e
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-16 10:27:04 -0300

    questao 3

---
+++ repetição/Questao3.java
@@ -0,0 +1,11 @@
+public class Questao3 {
+    public static void main(String[] args) {
+        int inicial = 0;
+
+        for(int i = 1; i < 101; i++) {
+            inicial += i;
+        }
+
+        System.out.println(inicial);
+    }
+}
