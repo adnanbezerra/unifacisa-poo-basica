babe568c26c7478db2627044fef3906916efcddd
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-23 09:40:14 -0300

    questao 2

---
+++ arrays/Questao2.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        String[] nomes = {"Adnan", "Natália", "Gabryell", "Caio", "Armando", "Armandois"};
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
