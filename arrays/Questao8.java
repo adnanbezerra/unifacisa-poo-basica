d5c769e666dd58c0306d702b3bcc2d865c4c8e53
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-23 09:41:02 -0300

    questao 8

---
+++ arrays/Questao8.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Questao8 {
+    public static void main(String[] args) {
+        String[] strings = {"Dena", "Lele", "Nata", "Narla", "Main"};
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + " | ");
+        }
+    }
+}
