f18f2b336a83abc0326bfa7733cd46ea3d8a0b1a
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-23 09:41:15 -0300

    questao 10

---
+++ arrays/Questao10.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class Questao10 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        matrix[0][0] = 1;
+        matrix[0][1] = 1;
+        matrix[0][2] = 1;
+
+        matrix[1][0] = 1;
+        matrix[1][1] = 1;
+        matrix[1][2] = 1;
+
+        matrix[2][0] = 1;
+        matrix[2][1] = 1;
+        matrix[2][2] = 1;
+    }
+}
