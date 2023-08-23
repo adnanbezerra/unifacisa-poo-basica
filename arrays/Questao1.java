d57f6003f90f17982525dd73dc64c7c8071810c7
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-23 09:39:58 -0300

    questão 1

---
+++ arrays/Questao1.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class Questao1 {
+    public static void main(String[] args) {
+        int[] primos = {3, 5, 7, 11, 13};
+
+        System.out.println(primos[0]);
+        System.out.println(primos[1]);
+        System.out.println(primos[2]);
+        System.out.println(primos[3]);
+        System.out.println(primos[4]);
+    }
+}
