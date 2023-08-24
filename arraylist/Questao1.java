d8c0de3c7ba3d01ccd77e79c635bbae1dc83fc0e
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-24 10:04:20 -0300

    questao 1

---
+++ arraylist/Questao1.java
@@ -0,0 +1,11 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Questao1 {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<Integer> ints = new ArrayList<>();
+        ArrayList<Float> floats = new ArrayList<>();
+    }
+}
