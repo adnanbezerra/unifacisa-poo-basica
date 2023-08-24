dc710318e0d7d843f01502a03889bf648db29b45
Adnan Bezerra <88043411+adnanbezerra@users.noreply.github.com>
2023-08-24 10:04:25 -0300

    questao 2

---
+++ arraylist/Questao2.java
@@ -0,0 +1,15 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<Integer> ints = new ArrayList<>();
+        ArrayList<Float> floats = new ArrayList<>();
+
+        strings.add("Olá meus amigos");
+        ints.add(1);
+        floats.add(Float.parseFloat("1.23"));
+    }
+}
